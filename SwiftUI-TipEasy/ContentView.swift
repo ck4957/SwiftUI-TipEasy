60b180d986f2fd58d3611e75dc5dc2e9afd35da6
Chirag Kular <ck4957@rit.edu>
2025-02-08 19:43:54 -0500

    Initial Commit

---
+++ SwiftUI-TipEasy/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SwiftUI-TipEasy
+//
+//  Created by Chirag Kular on 2/8/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SwiftUI-TipEasy/SwiftUI_TipEasyApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_TipEasyApp.swift
+//  SwiftUI-TipEasy
+//
+//  Created by Chirag Kular on 2/8/25.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_TipEasyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
